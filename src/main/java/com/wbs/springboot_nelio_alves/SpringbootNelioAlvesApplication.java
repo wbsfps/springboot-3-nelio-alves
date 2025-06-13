3aca13902eb6fa09bae333dd8d6c2dfee0eecc42
William Santana <williamandrade1058@gmail.com>
2025-06-13 19:15:36 -0300

    start project

---
+++ src/main/java/com/wbs/springboot_nelio_alves/SpringbootNelioAlvesApplication.java
@@ -0,0 +1,13 @@
+package com.wbs.springboot_nelio_alves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNelioAlvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNelioAlvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wbs/springboot_nelio_alves/SpringbootNelioAlvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbs.springboot_nelio_alves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNelioAlvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
