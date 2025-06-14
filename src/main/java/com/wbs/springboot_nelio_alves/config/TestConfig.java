1e6d35213abc598d683c9980a0b43b8f00cd6d46
William Santana <williamandrade1058@gmail.com>
2025-06-14 18:45:42 -0300

    Enum OrderStatus

--- src/main/java/com/wbs/springboot_nelio_alves/config/TestConfig.java
+++ src/main/java/com/wbs/springboot_nelio_alves/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.wbs.springboot_nelio_alves.config;
 
 import com.wbs.springboot_nelio_alves.entities.Order;
 import com.wbs.springboot_nelio_alves.entities.User;
+import com.wbs.springboot_nelio_alves.entities.enums.OrderStatus;
 import com.wbs.springboot_nelio_alves.repository.OrderRepository;
 import com.wbs.springboot_nelio_alves.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +27,9 @@ public class TestConfig implements CommandLineRunner {
         var user1 = new User(null, "William", "william@gmail.com", "79 999999999", "123456");
         var user2 = new User(null, "Bob", "bob@gmail.com", "55 888888888", "123456");
 
-        var order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        var order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        var order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        var order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+        var order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+        var order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
 
         userRepository.saveAll(Arrays.asList(user1, user2));
--- src/main/java/com/wbs/springboot_nelio_alves/entities/Order.java
+++ src/main/java/com/wbs/springboot_nelio_alves/entities/Order.java
@@ -1,6 +1,7 @@
 package com.wbs.springboot_nelio_alves.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.wbs.springboot_nelio_alves.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -20,16 +21,18 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order(){}
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -48,12 +51,22 @@ public class Order implements Serializable {
         this.moment = moment;
     }
 
-    public User getUser() {
+    public User getClient() {
         return client;
     }
 
-    public void setUser(User user) {
-        this.client = user;
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
