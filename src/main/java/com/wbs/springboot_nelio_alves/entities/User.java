e423992b3aa0691f712e35611abba22c56453f81
William Santana <williamandrade1058@gmail.com>
2025-06-13 23:02:23 -0300

    Banco de dados H2, test profile, JPA

--- src/main/java/com/wbs/springboot_nelio_alves/entities/User.java
+++ src/main/java/com/wbs/springboot_nelio_alves/entities/User.java
@@ -1,13 +1,19 @@
 package com.wbs.springboot_nelio_alves.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
